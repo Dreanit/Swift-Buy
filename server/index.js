674ffc72e9cce32c4e162dd53dc5796ed0d0e564
Sahil Agarwal <sahil.agarwal2021@vitbhopal.ac.in>
2023-01-25 15:37:56 +0530

    merge issue resolve

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const authRouter=require('./routes/auth');
 const adminRouter=require('./routes/admin');
 const productRouter=require('./routes/product');
 //init
-const PORT =3000;
+const PORT =8080;
 const app=express();
 const DB="mongodb+srv://dreanit:Sahil3005@cluster0.6be2l2u.mongodb.net/?retryWrites=true&w=majority";
 //middleware
